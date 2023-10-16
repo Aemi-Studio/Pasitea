ca0c779dbf716b5653491ba8c52cda368c12a59e
Gigi <42325924+g-cqd@users.noreply.github.com>
2023-10-16 19:26:57 +0200

    Initial Commit

---
+++ Negroni/NegroniApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegroniApp.swift
+//  Negroni
+//
+//  Created by Guillaume Coquard on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NegroniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
