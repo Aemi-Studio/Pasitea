0583c0924bb94a42c95c8bd18a377cc82638a42f
TheRealSalvo <salva0729@gmail.com>
2023-11-06 20:38:35 +0100

    function add button in CustomBackButton

--- Pasitea/View/Utils/Components/CustomBackButton.swift
+++ Pasitea/View/Utils/Components/CustomBackButton.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct CustomBackButton<Content: View>: View {
     @Environment(\.modelContext) var modelContext
 
-    @State private var infoPresented = false
-    @State private var alertPresented = false
-    @State private var isButtonPressed = false
+    @State private var infoPresented        = false
+    @State private var addViewIsPresented   = false
+    @State private var alertPresented       = false
+    @State private var isButtonPressed      = false
 
     var trackItem: TrackItem?
 
@@ -27,52 +28,56 @@ struct CustomBackButton<Content: View>: View {
 
     var body: some View {
         HStack {
-            if display.isSubset(of: .both) {
-                if display.contains(.info) {
-                    Button {
-                        isButtonPressed.toggle()
-                        infoPresented = true
-                    } label: {
-                        Label("Informations", systemImage: "info.circle")
-                            .customSingleBackButton()
-                    }
-                    .sheet(isPresented: $infoPresented) {
-                        content
-                    }
-                }
-                Spacer()
-                if display.contains(.close) {
-                    Button(role: .none) {
-                        isButtonPressed.toggle()
-                        if enforce {
-                            alertPresented = true
-                        } else {
-                            trackItem?.saveInto(modelContext)
-                            withAnimation {
-                                dismissAction?()
-                                customAction?()
-                                customDismiss?()
-                            }
-                        }
-                    } label: {
-                        Label("Close", systemImage: "xmark.circle.fill")
-                            .customSingleBackButton()
-                    }
-                }
-            } else if display.contains(.add) {
+            if display.isDisjoint(with: [.info,.close,.add]){
+                EmptyView()
+                    .frame(height: 48)
+            }
+            
+            if display.contains(.info) {
                 Button {
                     isButtonPressed.toggle()
                     infoPresented = true
                 } label: {
-                    Label("Add", systemImage: "plus.circle")
+                    Label("Informations", systemImage: "info.circle")
                         .customSingleBackButton()
                 }
                 .sheet(isPresented: $infoPresented) {
                     content
                 }
-            } else {
-                EmptyView()
-                    .frame(height: 48)
+            }
+            
+            if display.contains(.close) {
+                Spacer()
+                Button(role: .none) {
+                    isButtonPressed.toggle()
+                    if enforce {
+                        alertPresented = true
+                    } else {
+                        trackItem?.saveInto(modelContext)
+                        withAnimation {
+                            dismissAction?()
+                            customAction?()
+                            customDismiss?()
+                        }
+                    }
+                } label: {
+                    Label("Close", systemImage: "xmark.circle.fill")
+                        .customSingleBackButton()
+                }
+            }
+            
+            if display.contains(.add) {
+                Spacer()
+                Button {
+                    isButtonPressed.toggle()
+                    addViewIsPresented = true
+                } label: {
+                    Label("Add", systemImage: "plus")
+                        .customSingleBackButton()
+                }
+                .sheet(isPresented: $addViewIsPresented) {
+                    TrackItemAddView()
+                }
             }
         }
         .customHaptic(isButtonPressed)
